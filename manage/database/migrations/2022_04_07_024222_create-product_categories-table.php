ecd0c4cfa749fea51577d35873567f435c97972f
Mai Chiếm An <chieman2k3@gmail.com>
2022-04-07 11:58:46 +0700

    Dự Án Module 3

--- manage/database/migrations/2022_04_07_024222_create-product_categories-table.php
+++ manage/database/migrations/2022_04_07_024222_create-product_categories-table.php
@@ -13,7 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+       Schema::create('product_categories', function(Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+
+        });
     }
 
     /**
--- manage/database/migrations/2022_04_07_042046_create_product_logs_table.php
+++ manage/database/migrations/2022_04_07_042046_create_product_logs_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('product_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->string('content');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('product_logs');
     }
 };
